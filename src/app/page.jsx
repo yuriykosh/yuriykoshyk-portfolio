e9707178bc72355cb2b56692f0e93f90b208fe6d
Yuriy Koshyk <yuriy.koshyk@gmail.com>
2023-06-17 00:01:04 +0200

    commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,31 +10,33 @@ export default function Home() {
       headingStart: "UX/UI",
       headingEnd: "Design",
       icon: "carbon:navaid-ndb",
-      href: "/design",
-      color: "rgba(236, 104, 62, 0.8)",
+      href: "/portfolio/design",
+      color: "text-fox-500/80 group-hover:text-fox-500",
+      classes: "border-fox-500/80 hover:border-fox-500",
     },
     {
       id: 2,
       headingStart: "Frontend",
       headingEnd: "Development",
       icon: "carbon:navaid-tacan",
-      href: "/dev",
-      color: "rgba(86, 118, 191, 0.8)",
+      href: "/portfolio/dev",
+      color: "text-whale-500/80 group-hover:text-whale-500",
+      classes: "border-whale-500/80 hover:border-whale-500",
     },
   ];
 
   return (
     <div className="flex flex-col justify-between h-full ">
-      <h1 className="before:content-['Koshyk'] before:block before:absolute before:text-subtitle">
+      <h1 className="before:content-['Koshyk'] before:block before:absolute before:text-subtitle text-white">
         Yuriy
       </h1>
-      <div className="flex flex-col w-3/5 gap-4 ml-auto">
+      <div className="flex flex-col w-3/5 gap-4 ml-auto sm:w-2/5 md:w-1/2 xs:h-fit xs:gap-10">
         {category.map((item) => (
           <Category key={item.id} {...item} />
         ))}
       </div>
 
-      <BgPicture className=" w-[729px] h-[727px] -top-[54px] -left-[377px] lg:w-[1573px] lg:h-[1571px] lg:-top-[274px] lg:-left-[461px]" />
+      <BgPicture className=" w-[729px] h-[727px] -top-[54px] -left-[377px] xs:w-[630px] xs:h-[1034px] xs:-top-[76px] xs:-left-[325px] sm:w-[1106px] sm:h-[1034px] sm:-top-[76px] sm:-left-[572px] md:w-[1335px] md:h-[1341px] md:-top-[99px] md:-left-[690px] lg:w-[1573px] lg:h-[1571px] lg:-top-[274px] lg:-left-[461px]" />
     </div>
   );
 }
--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -12,19 +12,24 @@ const Category = ({
   href,
   color,
   className,
+  classes,
 }) => {
   return (
     <Link
       href={href}
-      className={`flex flex-col gap-3 py-4 px-6 border rounded-4xl backdrop-blur bg-black/25 hover:border-orange-500`}
-      style={{
-        borderColor: `${color}`,
-      }}
+      className={`group flex flex-col xs:justify-between gap-3 xs:gap-12 sm:gap-12 md:gap-14 py-4 md:py-6 px-6 border rounded-4xl backdrop-blur bg-black/25 transition ${classes}`}
+      // style={{
+      //   borderColor: `${color}`,
+      // }}
     >
-      <h4>
+      <h4 className="group-hover:text-white">
         {headingStart} <span className="block">{headingEnd}</span>
       </h4>
-      <Icon icon={icon} color={color} className="text-4xl" />
+      <Icon
+        icon={icon}
+        // color={color}
+        className={`text-[32px] xs:text-[40px] md:text-[66px]  ${color} transition group-hover:text-fox-500`}
+      />
     </Link>
   );
 };
