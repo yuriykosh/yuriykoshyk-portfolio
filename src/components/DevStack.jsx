bab7f1ae9e28c7ed3d0796c71ae59a246706fa87
Yuriy Koshyk <yuriy.koshyk@gmail.com>
2023-06-26 21:30:52 +0200

    commit

--- src/components/DevStack.jsx
+++ src/components/DevStack.jsx
@@ -1,44 +1,68 @@
 import {
-  IconToolAdobeAE,
-  IconToolAdobeIllustrator,
-  IconToolAdobeIndesign,
-  IconToolAdobePhotoshop,
-  IconToolAdobePremiere,
-  IconToolAdobeXD,
-  IconToolBlender,
+  IconToolHtml,
+  IconToolCss,
+  IconToolReact,
+  IconToolNextjs,
+  IconToolSass,
+  IconToolJavascript,
+  IconToolTypescript,
+  IconToolRedux,
+  IconToolVite,
+  IconToolWebpack,
+  IconToolNodejs,
+  IconToolGit,
+  IconToolPhp,
   IconToolFigma,
-  IconToolSketch,
+  IconToolAdobeCC,
 } from "@/utils/Icons";
 
 const DevStack = () => {
   return (
     <ul className="flex flex-wrap items-center gap-1 xl:gap-2">
       <li>
+        <IconToolHtml className="w-6 h-6 xs:w-10 xs:h-10 sm:w-12 sm:h-12 lg:w-10 lg:h-10 xl:w-12 xl:h-12 2xl:w-16 2xl:h-16 text-whale-500" />
+      </li>
+      <li>
+        <IconToolCss className="w-6 h-6 xs:w-10 xs:h-10 sm:w-12 sm:h-12 lg:w-10 lg:h-10 xl:w-12 xl:h-12 2xl:w-16 2xl:h-16 text-whale-500" />
+      </li>
+      <li>
+        <IconToolReact className="w-6 h-6 xs:w-10 xs:h-10 sm:w-12 sm:h-12 lg:w-10 lg:h-10 xl:w-12 xl:h-12 2xl:w-16 2xl:h-16 text-whale-500" />
+      </li>
+      <li>
+        <IconToolNextjs className="w-6 h-6 xs:w-10 xs:h-10 sm:w-12 sm:h-12 lg:w-10 lg:h-10 xl:w-12 xl:h-12 2xl:w-16 2xl:h-16 text-whale-500" />
+      </li>
+      <li>
+        <IconToolSass className="w-6 h-6 xs:w-10 xs:h-10 sm:w-12 sm:h-12 lg:w-10 lg:h-10 xl:w-12 xl:h-12 2xl:w-16 2xl:h-16 text-whale-500" />
+      </li>
+      <li>
+        <IconToolJavascript className="w-6 h-6 xs:w-10 xs:h-10 sm:w-12 sm:h-12 lg:w-10 lg:h-10 xl:w-12 xl:h-12 2xl:w-16 2xl:h-16 text-whale-500" />
+      </li>
+      <li>
+        <IconToolTypescript className="w-6 h-6 xs:w-10 xs:h-10 sm:w-12 sm:h-12 lg:w-10 lg:h-10 xl:w-12 xl:h-12 2xl:w-16 2xl:h-16 text-whale-500" />
+      </li>
+      <li>
+        <IconToolRedux className="w-6 h-6 xs:w-10 xs:h-10 sm:w-12 sm:h-12 lg:w-10 lg:h-10 xl:w-12 xl:h-12 2xl:w-16 2xl:h-16 text-whale-500" />
+      </li>
+      <li>
+        <IconToolVite className="w-6 h-6 xs:w-10 xs:h-10 sm:w-12 sm:h-12 lg:w-10 lg:h-10 xl:w-12 xl:h-12 2xl:w-16 2xl:h-16 text-whale-500" />
+      </li>
+      <li>
+        <IconToolWebpack className="w-6 h-6 xs:w-10 xs:h-10 sm:w-12 sm:h-12 lg:w-10 lg:h-10 xl:w-12 xl:h-12 2xl:w-16 2xl:h-16 text-whale-500" />
+      </li>
+      <li>
+        <IconToolNodejs className="w-6 h-6 xs:w-10 xs:h-10 sm:w-12 sm:h-12 lg:w-10 lg:h-10 xl:w-12 xl:h-12 2xl:w-16 2xl:h-16 text-whale-500" />
+      </li>
+      <li>
+        <IconToolGit className="w-6 h-6 xs:w-10 xs:h-10 sm:w-12 sm:h-12 lg:w-10 lg:h-10 xl:w-12 xl:h-12 2xl:w-16 2xl:h-16 text-whale-500" />
+      </li>
+      <li>
+        <IconToolPhp className="w-6 h-6 xs:w-10 xs:h-10 sm:w-12 sm:h-12 lg:w-10 lg:h-10 xl:w-12 xl:h-12 2xl:w-16 2xl:h-16 text-whale-500" />
+      </li>
+      <li>
         <IconToolFigma className="w-6 h-6 xs:w-10 xs:h-10 sm:w-12 sm:h-12 lg:w-10 lg:h-10 xl:w-12 xl:h-12 2xl:w-16 2xl:h-16 text-whale-500" />
       </li>
       <li>
-        <IconToolSketch className="w-6 h-6 xs:w-10 xs:h-10 sm:w-12 sm:h-12 lg:w-10 lg:h-10 xl:w-12 xl:h-12 2xl:w-16 2xl:h-16 text-whale-500" />
-      </li>
-      <li>
-        <IconToolBlender className="w-6 h-6 xs:w-10 xs:h-10 sm:w-12 sm:h-12 lg:w-10 lg:h-10 xl:w-12 xl:h-12 2xl:w-16 2xl:h-16 text-whale-500" />
-      </li>
-      <li>
-        <IconToolAdobeXD className="w-6 h-6 xs:w-10 xs:h-10 sm:w-12 sm:h-12 lg:w-10 lg:h-10 xl:w-12 xl:h-12 2xl:w-16 2xl:h-16 text-whale-500" />
-      </li>
-      <li>
-        <IconToolAdobePhotoshop className="w-6 h-6 xs:w-10 xs:h-10 sm:w-12 sm:h-12 lg:w-10 lg:h-10 xl:w-12 xl:h-12 2xl:w-16 2xl:h-16 text-whale-500" />
-      </li>
-      <li>
-        <IconToolAdobeIllustrator className="w-6 h-6 xs:w-10 xs:h-10 sm:w-12 sm:h-12 lg:w-10 lg:h-10 xl:w-12 xl:h-12 2xl:w-16 2xl:h-16 text-whale-500" />
-      </li>
-      <li>
-        <IconToolAdobeAE className="w-6 h-6 xs:w-10 xs:h-10 sm:w-12 sm:h-12 lg:w-10 lg:h-10 xl:w-12 xl:h-12 2xl:w-16 2xl:h-16 text-whale-500" />
-      </li>
-      <li>
-        <IconToolAdobePremiere className="w-6 h-6 xs:w-10 xs:h-10 sm:w-12 sm:h-12 lg:w-10 lg:h-10 xl:w-12 xl:h-12 2xl:w-16 2xl:h-16 text-whale-500" />
-      </li>
-      <li>
-        <IconToolAdobeIndesign className="w-6 h-6 xs:w-10 xs:h-10 sm:w-12 sm:h-12 lg:w-10 lg:h-10 xl:w-12 xl:h-12 2xl:w-16 2xl:h-16 text-whale-500" />
+        <IconToolAdobeCC className="w-6 h-6 xs:w-10 xs:h-10 sm:w-12 sm:h-12 lg:w-10 lg:h-10 xl:w-12 xl:h-12 2xl:w-16 2xl:h-16 text-whale-500" />
       </li>
     </ul>
   );
